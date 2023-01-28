a96bd9d4544c42e871bd4898fc63ec1ef636149f
rosbel <rosbelcr@gmail.com>
2023-01-27 22:48:42 -0500

    setting styles

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import React, {Component}from 'react';
-//import logo from './logo.svg';
-import './App.css';
-import Form from './Form';
+import React from 'react';
 
-class App extends Component{
-  state = {
-    fields:{}
-  }
-  onChange = updatedValue => {
-      //console.log('App Component got' , fields)
-      this.setState({fields: {
-        ...this.state.fields,
-        ...updatedValue
-      }
-      });
-  }
-  render(){
-    return (
-      <div className="App">
 
-        <h1> My Form </h1>
-        <Form  onChange ={fields => this.onChange(fields)}/>
-        <p>
-          {JSON.stringify(this.state.fields, null, 2)}
-        </p>
-      </div>
-    );
-  }
+function App(){
+  return(
+    <h1>Hola Mundo con React</h1>
+  )
 }
 
 export default App;
